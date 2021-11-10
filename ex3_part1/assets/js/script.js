15e5959258409bf6af2457ad3de0a82361df91fe
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-10 16:44:13 +0100

    Modification de l'ex3 (- '_' dans prompt) + ex en cours -

--- ex3_part1/assets/js/script.js
+++ ex3_part1/assets/js/script.js
@@ -1,4 +1,4 @@
 //boite de dialogue prompt pour remplir le prénom
-var firstName = prompt('Prénom :', '');
+var firstName = prompt('Prénom :');
 // créer une var correspondant au prompt pour pouvoir l'afficher dans l'alerte
 alert('Bonjour, ' + firstName)
--- ex6_part1/assets/js/script.js
+++ ex6_part1/assets/js/script.js
@@ -1,17 +1,10 @@
-var firstParagraph = document.getElementById('firstParagraph');
-//commentaire
-console.log()
+var button= document.getElementById('button');
+var feet = document.getElementById('feet');
+var year = document.getElementById('year');
 
-var age = 15;
+button.addEventListener('click', function getValue(){
+  var feet = document.getElementById('feet').value;
+  var year = document.getElementById('year').value;
+  alert('Voici le résultat de ce magnifique calcul x2 : ' + (feet*2) + '\nsi j y ajoute 5 : ' + (feet*2 + 5) + '\nsi je multiplie par 50 ce pied de Géant : ' + (feet*2 + 5)*50) + '\nj enleve l annee de naissance : ' + (feet*2+5)*50 - year;
+});
 
-if(age > 18){// si ma variable age est stritement supérieure à 18,{} sinon {}
-
-} else {
-    
-}
-
-if(age >= 18){ //supérieur ou égal à 18
-    alert('vous êtes majeur')
-} else {
-  alert('vous êtes mineur')
-}

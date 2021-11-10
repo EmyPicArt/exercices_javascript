b8ad8c55b132afb19a347ca85eb72b5ddba0281d
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-10 11:40:03 +0100

    création des dossiers

---
+++ ex8/assets/js/script.js
@@ -0,0 +1,17 @@
+var firstParagraph = document.getElementById('firstParagraph');
+//commentaire
+console.log()
+
+var age = 15;
+
+if(age > 18){// si ma variable age est stritement supérieure à 18,{} sinon {}
+
+} else {
+    
+}
+
+if(age >= 18){ //supérieur ou égal à 18
+    alert('vous êtes majeur')
+} else {
+  alert('vous êtes mineur')
+}

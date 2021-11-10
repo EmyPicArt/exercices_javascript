9426e362b8f00de243d2727036ef83f9a9d1b8d2
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-10 11:51:16 +0100

    fin ex1
    var lastName avec affichage alert

--- ex1_part1/assets/js/script.js
+++ ex1_part1/assets/js/script.js
@@ -1,17 +1,2 @@
-var firstParagraph = document.getElementById('firstParagraph');
-//commentaire
-console.log()
-
-var age = 15;
-
-if(age > 18){// si ma variable age est stritement supérieure à 18,{} sinon {}
-
-} else {
-    
-}
-
-if(age >= 18){ //supérieur ou égal à 18
-    alert('vous êtes majeur')
-} else {
-  alert('vous êtes mineur')
-}
+var lastName = 'PICART';
+alert(lastName)

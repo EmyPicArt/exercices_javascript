d5aebdfcb93c86f2f001e910ede92c342ba895d0
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-12 09:23:10 +0100

    fin ex 6 - les calculs
    Modification du html ex 4 (erreur de body et html en trop)

--- ex6_part1/assets/js/script.js
+++ ex6_part1/assets/js/script.js
@@ -2,18 +2,16 @@ var button= document.getElementById('button');
 var feet = document.getElementById('feet');
 var year = document.getElementById('year');
 
-button.addEventListener('click', function getValue(){
-  var feet = document.getElementById('feet').value;
-  var year = document.getElementById('year').value;
-  alert('Voici le résultat de ce magnifique calcul x2 : ' + (feet*2) + '\nsi j y ajoute 5 : ' + (feet*2 + 5) + '\nsi je multiplie par 50 ce pied de Géant : ' + (feet*2 + 5)*50);
-});
 
 button.addEventListener('click', function getValue(){
   var feet = document.getElementById('feet').value;
   var year = document.getElementById('year').value;
-  alert('j enleve l annee de naissance : ' + (((feet*2+5)*50) - year)) 
+  alert(((((feet*2+5)*50) - year)+ 1769)) 
 });
 
-// le code fonctionne jusqu'à enlever l'année de naissance, tester add 1769
-// + '\nj ajoute au tout 1769 sans savoir pourquoi : ' + (((feet*2+5)*50) - year) + 1769;
 
+// button.addEventListener('click', function getValue(){
+//   var feet = document.getElementById('feet').value;
+//   var year = document.getElementById('year').value;
+//   alert('Voici le résultat de ce magnifique calcul x2 : ' + (feet*2) + '\nsi j y ajoute 5 : ' + (feet*2 + 5) + '\nsi je multiplie par 50 ce pied de Géant : ' + (feet*2 + 5)*50) + 'j enleve l annee de naissance : ' + ((((feet*2+5)*50) - year) + 'si j ajoute 1769 + + ((((feet*2+5)*50) - year)+ 1769);
+// });

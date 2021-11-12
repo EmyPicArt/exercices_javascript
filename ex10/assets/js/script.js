4c206d37d98622c226a0673c0426a36426f45380
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-12 17:06:37 +0100

    test en cours ex 10

--- ex10/assets/js/script.js
+++ ex10/assets/js/script.js
@@ -1,17 +1,17 @@
-var firstParagraph = document.getElementById('firstParagraph');
-//commentaire
 console.log()
+var password = document.getElementById('password').value;
+var truePassword = '0000';
+var userName = document.getElementById('userName').value;
+var sucess = document.getElementById('alert-sucess');
+var danger = document.getElementById('alert-danger');
 
-var age = 15;
-
-if(age > 18){// si ma variable age est stritement supérieure à 18,{} sinon {}
-
-} else {
-    
-}
-
-if(age >= 18){ //supérieur ou égal à 18
-    alert('vous êtes majeur')
-} else {
-  alert('vous êtes mineur')
-}
+userName.addEventListener("keyup", function(){
+  if ('password' === 'truePassword'){
+    password.style.backgroundColor = green;
+    empty.innerHTML ='<div id="liveAlertPlaceholder" class="alert alert-success">Good !</div>';
+  } else {
+    password.style.backgroundColor = red;
+    empty.innerHTML = '<div id="liveAlertPlaceholder" class="alert alert-danger">No good !</div>';
+  }
+});
+// voir le bg color et le innerhtml
--- ex5_part1/assets/js/script.js
+++ ex5_part1/assets/js/script.js
@@ -6,4 +6,18 @@ button.addEventListener('click', function getValue(){
   alert('Il va falloir se battre il ne reste que :' + cookies % invited +' cookie(s) !!!');
 });
 
+//Methode avec arguments
+
+// function modulo(toto, tata){
+//   return toto % tata
+// }
+
+// alert(modulo (10, 3));
+
+// //Methode sans arguments
+
+// document.getElementById('button').addEventListener('click', function modulo(){
+//   alert('Reste de la division : ' + document.getElementById('firstNumber').value % document.getElementById('secondNumber').value)
+// })
+
 

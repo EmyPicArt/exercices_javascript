60293b99adcfa2a681b8effc6c02746d138b7aa2
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-15 14:38:18 +0100

    correction formateur ex7 ajoutée
    Mettre une sécurité : contrôle une première condition générale qui si elle est respectée entraine la lecture des lignes suivantes (condition dans une condition)

--- ex7_part1/assets/js/script.js
+++ ex7_part1/assets/js/script.js
@@ -1,15 +1,44 @@
-//commentaire
-console.log()
-var button = document.getElementById('button');
+// Ma méthode :
+// var button = document.getElementById('button');
 
 
-var recupValue = function(){
-  var age = document.getElementById('age').value;
-  if(age >= 18){ //supérieur ou égal à 18
-      alert('vous êtes majeur')
-  } else {
-    alert('vous êtes mineur')
-  }
-}
+// var recupValue = function(){
+//   var age = document.getElementById('age').value;
+//   if(age >= 18){ //supérieur ou égal à 18
+//       alert('vous êtes majeur')
+//   } else {
+//     alert('vous êtes mineur')
+//   }
+// }
 
-button.addEventListener('click', recupValue);
+// button.addEventListener('click', recupValue);
+
+// correction :
+var btn = document.getElementById('button');
+
+btn.addEventListener('click', function(){
+    var age = document.getElementById('age').value;
+    
+//     //1ère méthode
+//     // if(age >= 18 && age <= 120){ (= si age supérieur ou égal à 18 ET age inférieur ou égal à 120)
+//     //     alert('Vous êtes majeur !');
+//     // } else if(age > 0 && age < 18) { (=SINON et on ajoute une condition : age compris entre 0 et 18)
+//     //     alert('Vous êtes mineur !')
+//     // } else {(=SINON message d'erreur car entrée incorrecte)
+//     //     alert('Arrête tes conneries !');
+//     // }
+
+
+
+//     //2ème méthode : meilleure car pose une une 1ere condition générale qui si elle n'est pas respectée affiche une erreur
+    if(age > 0 && age <= 120){ 
+      // (=vérifie que l'age est entre 0 et 120 / évite age négatif ou + de 121)
+        if(age >= 18 && age <= 120){
+            alert('Vous êtes majeur !');
+        } else {
+            alert('Vous êtes mineur !');
+        }
+    } else {
+        alert('Arrête tes conneries !');
+    }
+})
